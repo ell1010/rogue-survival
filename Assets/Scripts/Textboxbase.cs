9a27acf2ce753b0c89272016a73f619c38549dd7
ell_1010 <elliottbarrett25@gmail.com>
2019-02-21 17:25:29 +0000

    progress on textbox and pickup system

--- Assets/Scripts/Textboxbase.cs
+++ Assets/Scripts/Textboxbase.cs
@@ -14,13 +14,15 @@ public class Textboxbase : MonoBehaviour {
 	bool key;
 	bool keyt;
 	public bool finishedtyping = false;
+    bool skip;
 
 	// Use this for initialization
 	void Start () {
-		tbcontent = "This is a text box";
+		tbcontent = "This is a text box that has a lot of text ot give me time to test different things while the text is typing";
+        tbtext = this.gameObject.transform.GetChild(0).gameObject.GetComponent<Text>();
+        starttext();
+    }
 
-		
-	}
 	
 	// Update is called once per frame
 	void Update () 
@@ -29,52 +31,66 @@ public class Textboxbase : MonoBehaviour {
 	}
 	void starttext()
 	{
-		if (settingsmanager.instance.DownPressed()) 
-		{
-			if (!key && trigger) 
-			{
-				print ("true");
-				key = true;
-				keyt = true;
-			}
-		}
-		if(!settingsmanager.instance.DownPressed())
-		{
-			key = false;
-		}
-		if (keyt) {
-			if (!textbox.activeInHierarchy)
-			{
-				textbox.SetActive (true);
-			} else if (textbox.activeInHierarchy) 
-			{
-				if (!istyping && !finishedtyping) {
-					StartCoroutine (typewriter ());
-					istyping = true;
-					keyt = false;
-				} else if (istyping && !canceltyping) 
-				{
-					canceltyping = true;
-					print ("stop");
-					keyt = false;
-				}
-				if (finishedtyping) 
-				{
-					textbox.SetActive (false);
-					StopCoroutine (typewriter ());
-					tbtext.text = "";
-					keyt = false;
-					finishedtyping = false;
-				}
-			}
-		}
-		if (!trigger) 
-		{
-			textbox.SetActive (false);
-			StopCoroutine (typewriter ());
-			tbtext.text = "";
-		}
+        StartCoroutine(typewriter());
+
+
+		//if (settingsmanager.instance.DownPressed()) 
+		//{
+		//	if (!key && trigger) 
+		//	{
+		//		print ("true");
+		//		key = true;
+		//		keyt = true;
+		//	}
+		//}
+		//if(!settingsmanager.instance.DownPressed())
+		//{
+		//	key = false;
+		//}
+		//if (keyt) {
+		//	if (!textbox.activeInHierarchy)
+		//	{
+		//		textbox.SetActive (true);
+		//	} else if (textbox.activeInHierarchy) 
+		//	{
+		//		if (!istyping && !finishedtyping) {
+		//			StartCoroutine (typewriter ());
+		//			istyping = true;
+		//			keyt = false;
+		//		} else if (istyping && !canceltyping) 
+		//		{
+		//			canceltyping = true;
+		//			print ("stop");
+		//			keyt = false;
+		//		}
+		//		if (finishedtyping) 
+		//		{
+		//			textbox.SetActive (false);
+		//			StopCoroutine (typewriter ());
+		//			tbtext.text = "";
+		//			keyt = false;
+		//			finishedtyping = false;
+		//		}
+		//	}
+		//}
+		//if (!trigger) 
+		//{
+		//	textbox.SetActive (false);
+		//	StopCoroutine (typewriter ());
+		//	tbtext.text = "";
+		//}
 	}
+    public void skiptext()
+    {
+        if (istyping)
+            canceltyping = true;
+        else
+        {
+            tbtext.text = "";
+            gameObject.SetActive(false);
+        }
+
+    }
 	public IEnumerator typewriter()
 	{
 		int letter = 0;
